e2649392c55d381607f4a6bb82a2d6327f281061
itaiglikman <itaig1998@gmail.com>
2025-08-14 20:50:14 +0300

    feat: implement loader

--- src/Components/HomeArea/TwittsList/TwittsList.tsx
+++ src/Components/HomeArea/TwittsList/TwittsList.tsx
@@ -1,14 +1,24 @@
+import { Loader } from "@mantine/core";
 import { useTwitterContext } from "../../../Lib/Context/TwitterContext";
 import { TwittCard } from "../TwittCard/TwittCard";
 import "./TwittsList.css";
 
 export function TwittsList() {
-    const [twitts, setTwitts] = useTwitterContext()
+    const [twitts] = useTwitterContext();
+    const sortedTwitts = twitts.sort((a, b) =>
+        new Date(b.date).getTime() - new Date(a.date).getTime()
+    );
+
     return (
         <div className="TwittsList">
-            {twitts.map(t =>
-                <TwittCard key={t.id} twitt={t} />
-            )}
+            {/* should display when there is no twitts variable. 
+            when no length should display message 'no tweets  */}
+            {!twitts.length && <Loader />}
+            <div className="cards-container">
+                {sortedTwitts.map(t =>
+                    <TwittCard key={t.id} twitt={t} />
+                )}
+            </div>
 
         </div>
     );
