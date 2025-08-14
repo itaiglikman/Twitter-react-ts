cff60b6680c2149c4c41dcda9681ae4df4cf0db1
itaiglikman <itaig1998@gmail.com>
2025-08-14 14:38:06 +0300

    chagne: app and routing layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,12 @@
-import { Route, Routes } from "react-router-dom";
 import "./App.css";
-import { Header } from "./Components/Header/Header";
-import { HomePage } from "./Pages/HomePage/HomePage";
-import { ProfilePage } from "./Pages/ProfilePage/ProfilePage";
+import { Header } from "./Components/LayoutArea/Header/Header";
+import { Routing } from "./Components/LayoutArea/Routing/Routing";
 
 function App() {
     return (
         <div className="App">
             <Header />
-            <Routes>
-                <Route path="/" element={<HomePage />} />
-                <Route path="/profile" element={<ProfilePage />} />
-            </Routes>
+            <Routing />
         </div>
     );
 }
