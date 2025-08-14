4f38cbfe289ebac2bdc12a3535df6a20f15020af
itaiglikman <itaig1998@gmail.com>
2025-08-14 12:51:25 +0300

    feat: routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,19 @@
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
+import { Header } from "./Components/Header/Header";
+import { HomePage } from "./Pages/HomePage/HomePage";
+import { ProfilePage } from "./Pages/ProfilePage/ProfilePage";
 
 function App() {
-  return (
-    <div className="App"></div>
-  );
+    return (
+        <div className="App">
+            <Header />
+            <Routes>
+                <Route path="/" element={<HomePage />} />
+                <Route path="/profile" element={<ProfilePage />} />
+            </Routes>
+        </div>
+    );
 }
 
 export default App;
