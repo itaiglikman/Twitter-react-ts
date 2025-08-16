25b196086982e586b27834b47556784b857154d5
itaiglikman <itaig1998@gmail.com>
2025-08-16 23:43:52 +0300

    feat: page not found

--- src/Components/LayoutArea/Routing/Routing.tsx
+++ src/Components/LayoutArea/Routing/Routing.tsx
@@ -1,22 +1,20 @@
 import { Box } from "@mantine/core";
 import { Route, Routes } from "react-router-dom";
-import { HomePage } from "../../../Pages/HomePage/HomePage";
-import { ProfilePage } from "../../../Pages/ProfilePage/ProfilePage";
-import "./Routing.css";
-import { Login } from "../../AuthArea/Login/Login";
 import { Pages } from "../../../Lib/Types/types";
+import { HomePage } from "../../../Pages/HomePage/HomePage";
+import { PageNotFound } from "../../../Pages/PageNotFound/PageNotFound";
+import { Login } from "../../AuthArea/Login/Login";
 import { Register } from "../../AuthArea/Register/Register";
+import "./Routing.css";
 
 export function Routing() {
     return (
         <Box className="Routing" p={'sm'} w={'100%'}>
             <Routes>
                 <Route path={Pages.Home} element={<HomePage />} />
-                {/* <Route path="/profile" element={<ProfilePage />} /> */}
                 <Route path={Pages.Login} element={<Login />} />
                 <Route path={Pages.Register} element={<Register />} />
-
-                {/* add 404 */}
+                <Route path="*" element={<PageNotFound />} />
             </Routes>
         </Box>
     );
